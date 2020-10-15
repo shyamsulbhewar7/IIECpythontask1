7df3a6e437f6f446dddb31fa6481223e4bfd6836
shyamwin <shyamsulbhewar@gmail.com>
2020-10-15 18:13:42 +0000

    Perfect aahe hai editor

---
+++ example.py
@@ -0,0 +1,6 @@
+#print("Hello yar, Tu changal aahes pan thoda mistake zala aahet ata tula tyamadhe time nahi waste karacha , aata fakt study ani developement")
+def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+
+git_opeation()
+
